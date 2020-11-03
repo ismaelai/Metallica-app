484a0ca62cb26def916ac537dae8caa31e8846e5
Ismael Gonzalez <ismamontiel@hotmail.com>
2020-11-03 10:14:34 +0100

    x

--- client/src/lib/fetch-albums.js
+++ client/src/lib/fetch-albums.js
@@ -1,6 +1,6 @@
 const { REACT_APP_SERVER_URL } = process.env;
 
-export const getAllTracks = async () => {
+export const getAllAlbums = async () => {
   const response = await fetch(`${REACT_APP_SERVER_URL}/albums`);
   return await response.json();
 };
--- client/src/pages/playlist.js
+++ client/src/pages/playlist.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocalStorage } from '../lib/local-storage-hook.js';
+import { getTracksByIds } from '../lib/fetch-tracks.js';
+
 
 const Playlist = () => {
+  const [playlist, setPlaylist] = useLocalStorage('userCollection', []);
+  const [favorites, setFavorites] = useState([]);
+
+  const fetchTracks = async () => {
+    const tracksData = await getTracksByIds(playlist);
+    setPlaylist(tracksData);
+    console.log({ tracksData });
+  };
+
+  useEffect(() => {
+    fetchTracks();
+  }, []);
+
+
   return (
-    <main className="page playlist">
+    <main className="playlist">
       <h2>Playlist page</h2>
+
+      <section>
+        {favorites.map((track) => {
+          return (
+            <article key={track._id}>
+              <h3>{track.title}</h3>
+            </article>
+          );
+        })}
+      </section>
     </main>
   );
 };
